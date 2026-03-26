88368fa05c2124bbc86ed0621860eec18ffda128
Taran Govindu <trex.taran@gmail.com>
2026-03-25 19:33:31 -0700

    feat: setup CFD tool imports and initial class

---
+++ physics_cores/ansys_fluent/cfd_tool.py
@@ -0,0 +1,10 @@
+"""
+Fluidist Tool (CFD) for Aero-Structural Optimization System
+"""
+import os
+import csv
+import ansys.fluent.core as pyfluent
+
+class FluidistAgent:
+    """Agent responsible for running 2D aerodynamic analysis via Ansys Fluent."""
+    pass
